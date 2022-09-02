245ca8a26dc163a548d17ba9a80e752ea9355e3b
aluno <aluno@email.com>
2022-09-02 10:16:00 -0300

    mensagem

---
+++ text.js
@@ -0,0 +1,193 @@
+const pokemonAgua = [
+  {
+    nome: 'Seadra',
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/seadra-150x150.jpeg',
+    descrição: `É uma espécie de Pokémon do tipo (Água),introduzida na 1ª Geração e classificada oficialmente como Dragon Pokémon. O contato com o ele pode envenenar o inimigo em batalha. `,
+  },
+
+  {
+    nome: 'Lapras',
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/lapras-150x150.jpeg',
+    descrição: `É um Pokémon usado como meio de transporte por humanos. Eles cantam uma incrível melodia quando estão de bom humor. São dóceis e raramente lutam, mas sua doçura tornou-se um alvo fácil para caça, por isso Lapras é um Pokémon agora ameaçado de extinção.`,
+  },
+
+  {
+    nome: 'Wooper',
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/wooper-150x150.png',
+    descrição: `É um anfíbio Pokémon azul. Ele fica em pé sobre seus pequenos pés, tem uma grande cauda azul, e tambem guelras saindo dos lados de sua cabeça, e o que parece azul escuro, curvado marcas em seu torso.`,
+  },
+
+  {
+    nome: 'Lotad',
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/lotad-150x150.png' ,
+    descrição: `Vive em lagoas e lagos com água limpa, onde flutuam na superfície. Ele fica fraco se sua folha larga morrer. Em raras ocasiões, este Pokémon viaja em terra em busca de água limpa. Algumas lendas dizem que esse Pokémon vivia em terra firme.` ,
+  },
+];
+
+const pokemonFogo = [
+  {
+    nome: 'Ninetales' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/ninetales-150x150.jpeg' ,
+    descrição: `Lança uma luz sinistra de seus olhos vermelhos fulmejantes para ganhar o controle da mente de seus adversários. Dizem que este Pokémon vive por volta de 1000 anos. ` ,
+  },
+
+  {
+    nome: 'Torkoal' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/torkoal-150x150.png' ,
+    descrição: `Pokémon 3 gerações no número 324 em Pokedex. Ele vive na região de Hoenn e é do tipo ardente. Este Pokémon não evolui. Ele pode ser encontrado em áreas residenciais, climas quentes e praias. Há a chance de pegá-lo em parques, às vezes em estacionamentos e postos de gasolina, bem como em estádios e em áreas próximas a vulcões.3
+    Raramente aparece no mapa. Muitos jogadores o pegaram em alguns evento. ` ,
+  },
+
+  {
+    nome: 'Flareon' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/flareon-150x150.jpeg' ,
+    descrição: ` O pelo macio dele tem um propósito funcional de liberar calor no ar para que seu corpo não superaqueça. A temperatura corporal deste Pokémon pode atingir um máximo de 900 °C. ` ,
+  },
+
+  {
+    nome: 'Quilava' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/quilava-150x150.png' ,
+    descrição: ` É um Pokémon tipo fogo. Ele é a forma evoluida de Cyndaquil quando chega no nivel 14 e depois evolui para Typhlosion quando chega no nivel 36. ` ,
+  },
+];
+
+const pokemonEletrico = [
+   {
+    nome: 'Jolteon' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/jolteon-150x150.jpeg' ,
+    descrição: ` É um Pokémon canino que está coberto de pêlo amarelo com uma franja espetada em torno de sua cauda, e um colar branco em torno de seu pescoço. Suas orelhas são grandes e pontudas, e seus olhos e nariz pequeno são negros. ` ,
+  },
+
+  {
+    nome: 'Minun' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/minun-150x150.png' ,
+    descrição: ` É um pequeno Pokémon rato com amarelo como cor predominante. Suas grandes orelhas possuem uma grande parte azul, enquanto as partes amarelas são bem pequenas e têm um formato circular. ` ,
+  },
+
+  {
+    nome: 'Rotom' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/rotom-150x150.png' ,
+    descrição: ` Este Pokémon troca o seu tipo secundário quando habita eletrônicos e, normalmente, essa tipagem tem algo relacionado ao aparelho: A máquina de lavar dá a ele o tipo aquático; a máquina de cortar grama dá o tipo grama; o ventilador dá o tipo voador; o forno dá o tipo fogo; e a geladeira dá o tipo gelo. ` ,
+  },
+
+  {
+    nome: 'Luxray' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/luxray-150x150.png' ,
+    descrição: ` São extremamente cruéis e predatoriais, embora quando domesticado e treinado corretamente eles podem se tornar muito leais e fieis, protegendo seu treinador do perigo. Eles tendem a ser teimosos e podem administrar quantidades extremas de energia através de sua eletricidade. ` ,
+  },
+
+];
+
+const pokemonPlanta = [
+   {
+    nome: 'Oddish' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/oddish-150x150.jpeg' ,
+    descrição: ` Procura solo fértil e rico em nutrientes para se plantar. Durante o dia, enquanto é plantado, acredita-se que os pés deste Pokémon mudam de forma e se tornam semelhantes às raízes das árvores, além de suas folhas ficarem mais brilhantes. Se exposto ao luar, ele começa a se mover. ` ,
+  },
+
+  {
+    nome: 'Bellossom' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/bellossom-150x150.png' ,
+    descrição: ` É uma espécie de Pokémon do tipo Grass 8ic.png (Planta), introduzida na 2ª Geração e classificada oficialmente como Flower Pokémon. É uma das formas evoluídas de Gloom e formas finais de Oddish. ` ,
+  },
+
+  {
+    nome: 'Meganium' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/meganium-150x150.png' ,
+    descrição: ` Pokemon erva que tem a aparência de um apatossauro. Ele é verde claro, tem duas antenas amarelas na parte frontal do rosto, os olhos são de cor amarela e ficam nos lados da cabeça, e ao invés de ter folhas como os seus estágios anteriores, há grandes pétalas de flor em volta de seu pescoço longo. Diferente do estágio anterior, seu corpo ficou mais espesso além do pescoço e suas patas agora têm mais largura e estão com três garras. As pétalas em seu pescoço são especiais, pois soltam um aroma refrescante que acalma até os sentimentos mais agressivos e esse aroma também revive vegetações mortas. ` ,
+  },
+
+  {
+    nome: 'Celebi' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/celebi-150x150.png' ,
+    descrição: ` Pokémon mítico de dual-type Psíquico/Grama introduzido na Geração II. Não é conhecido para evoluir para ou de qualquer outro Pokémon. Ele serve como o guardião de Ilex Forest e só pode ser pego da natureza em Pokémon Crystal com um evento ativado pela GS Ball. ` ,
+  },
+
+];
+
+const pokemonVoador = [
+   {
+    nome: 'Dodrio' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/dodrio-150x150.jpeg' ,
+    descrição: ` Fique ligado se as três cabeças deste Pokémon estiverem cada uma olhando para uma direção diferente. É um claro sinal de que ele está com a guarda alta. Não cheque perto dele quando este Pokémon estiver assim, se não há grandes chances de você ser bicado. ` ,
+  },
+
+  {
+    nome: 'Gyarados' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/gyarados-150x150.jpeg' ,
+    descrição: ` Introduzida na 1ª Geração e classificada oficialmente como Atrocious Pokémon. É a forma evoluída e final de Magikarp. Ele tem a capacidade de Mega Evolução, e quando exposto à pedra Mega Stone Gyaradosite, assume sua forma Mega. ` ,
+  },
+
+  {
+    nome: 'Dragonite' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2016/08/dragonite-150x150.jpeg' ,
+    descrição: ` É um pokémon do tipo Dragão/Voador e é muito raro. Apesar de ser grande, ele é incrivelmente ágil e rápido. Esses Pokémons tem uma tendência a ter um bom coração e dizem ter inteligência semelhante a humana. ` ,
+  },
+
+  {
+    nome: 'Togetic' ,
+    link: 'https://www.geekno.com/pt/wp-content/uploads/sites/2/2018/10/togetic-150x150.png' ,
+    descrição: ` É um Pokémon tipo Normal e Voador. Ele é a forma evoluida de Togepi, quando chega num certo nivel de amizade com o seu treinador e depois evolui para Togekiss quando segura um Shiny Stone. ` ,
+  },
+
+];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const menu = document.querySelector('#menu');
+// const grupo = document.querySelectorAll('.group');
+// const sub = document.querySelectorAll('#sub-menu');
+
+// document.querySelectorAll('.item').forEach((e,i) => {
+
+// e.addEventListener('click', () =>{
+// if (i==0) {
+// menu.classList.add('hidden');
+// grupo[1].classList.add('active');
+
+// }else if(i==1){
+// menu.classList.add('hidden');
+// grupo[i].classList.add('active');
+// }else{
+// alert(`Abre link:${e.innerHTML}`);
+// }
+// });
+// });
+
+// const sub1 = document.querySelectorAll('#sub-menu .hidden');
+// sub1.forEach()
+
+// document.querySelectorAll('#sub-menu .item').forEach((e,i) => e.addEventListener('click', () => {
+//     document.querySelector('#menu').classList.remove('hidden');
+//     document.querySelector('#sub-menu .group.active').classList.remove('active');
+//     alert(`Abre link: ${e.innerHTML}`);
+// }));
