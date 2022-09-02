04252c6f59ee5c5a65f9579eb1aa1af8dec0833b
aluno <aluno@email.com>
2022-09-02 10:43:20 -0300

    alteração

--- ex.js
+++ ex.js
@@ -1,15 +1,10 @@
 const button = document.querySelector('button');
 
 button.addEventListener('click', () => {
+    const nome = document.querySelector('#nome').value;
     const user = document.querySelector('#user').value;
     const pass = document.querySelector('#pass').value;
 
-    // console.log(user, pass);
-    if ((user == 'aluno' && pass == 'undererere123') || (user == 'professor' && pass == 'admin789')) {
-        document.querySelector('#box').innerHTML = `<h2><span id="highlight">${user}</span> logado com sucesso.</h2>`;
-    }
-    else {
-        document.querySelector('#message').innerHTML = 'Usuário ou senha inválidos';
-    }
-    
+ 
+   
 });
